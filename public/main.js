556197ea89aa7819d660873cd9fcf714d3e1438b
pete gamache <gamache@gmail.com>
2011-04-24 19:58:11 -0400

    go-live content

--- public/main.js
+++ public/main.js
@@ -1,23 +1,31 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
+  window.onpopstate = function(event) {
+    if (event.state && event.state.title) {
+      $('#left').html(event.state.html);
+      document.title = event.state.title;
+    }
+  };
 
   $('#nav a').click(function(){
     var page = $(this).attr('href').replace(/^\//,'');
     if (page == '') return true; // tumblr feed js doesn't work with ajax
 
     var title = 'Missing Ships - ' + $(this).html();
-    if (window.history && history.pushState) 
-      window.history.pushState('hi mom!', title, '/'+page);
-    document.title = title;
 
     $.ajax({
-      url: page,
+      url: '/'+page,
       data: {partial: true},
       success: function(data){
         $('#left').html(data);
+        document.title = title;
+        if (window.history && window.history.pushState)
+          window.history.pushState({title: title, html: $('#left').html()},
+                                   title, '/'+page);
       }
     });
+
     return false;
   });
 
