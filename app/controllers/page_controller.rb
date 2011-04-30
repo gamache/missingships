7b6f20f0e517dfaf49adca17d7fb3b5f12b4ca14
pete gamache <gamache@gmail.com>
2011-04-30 09:33:08 -0400

    added mailing list form

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,16 @@
 class PageController < ApplicationController
   def page
     @page = params[:page] || 'index'
+
+    if params[:email]
+      @email = params[:email]
+      @name = params[:name]
+      addr = EmailAddress.new(:email => @email, :name => @name)
+      flash[:email_signup_message] = addr.save ?
+        "#{@email} has been added to the Missing Ships mailing list." :
+        "Please enter a valid email to sign up for the Missing Ships mailing list."
+    end
+
     render :template => "page/#{@page}", :layout => !params[:partial]
   end
 end
