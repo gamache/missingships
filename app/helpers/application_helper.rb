64a6f61712447cbba3a6b832a065578460298bab
pete gamache <pete@where.com>
2012-03-04 12:24:49 -0500

    upgrade to rails 3.1 and haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def navbar
+    { '/' => 'News',
+      '/bio' => 'Bio',
+      '/audio' => 'Audio',
+      '/pics' => 'Pics',
+      '/shows' => 'Shows',
+      '/contact' => 'Contact'
+    }.map{|url,name|
+      pg = @page.gsub(/index/, '')
+      active = pg==url[1..-1]
+      %Q{<li><a href="#{url}" id="nav_#{name}" #{' class="active"' if active}>#{name}</a></li>}
+    }.join(" ")
+  end
 end
